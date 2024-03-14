dae99a99fb269b84f5cfbf7e964c99aaec69379d
gsalgado <gsalgado@umich.edu>
2024-03-14 11:57:10 -0400

    changing the button colors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,67 @@
 /*  NEEDED */
 import { useState, useEffect } from "react"
-import { Navbar, Container, Nav } from "react-bootstrap"
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { HashLink } from 'react-router-hash-link';
+import {
+    BrowserRouter as Router
+} from "react-router-dom";
 
 //
 
-
-// Functional Component:
 export const NavBar = () => {
+
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
-            if (window.scrollY > 50) { setScrolled(true); }
-            else { setScrolled(false); }
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
         }
+
         window.addEventListener("scroll", onScroll);
+
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
-    const onUpdateActiveLink = (value) => { setActiveLink(value); }
-    // LEFT OFF HERE AT 11:30
-
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={''} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
+        <Router>
+            <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img src={''} alt="Logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <span className="navbar-text">
-                        <button className="vvd" onClick={() => console.log('aboutMe')}><span>About Me</span></button>
-                        <button className="vvd" onClick={() => console.log('technicalExperience')}><span>Technical Experience</span></button>
-                        <button className="vvd" onClick={() => console.log('myCommunities')}><span>My Communities</span></button>
-                        <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <span className="navbar-text">
+                            <HashLink to='#aboutMe'>
+                                <button className="vvd"><span>About Me</span></button>
+                            </HashLink>
+
+                            <HashLink to='#technicalExperience'>
+                                <button className="vvd"><span>Technical Experience</span></button>
+                            </HashLink>
+                            <HashLink to='#myCommunities'>
+                                <button className="vvd"><span>My Communities</span></button>
+                            </HashLink>
+
+                            <HashLink to='#connect'>
+                                <button className="vvd"><span>Let’s Connect</span></button>
+                            </HashLink>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Router>
     )
 }
