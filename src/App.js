4a8732d2d2d51841485ca0868ac5684ddccc6014
gsalgado <gsalgado@umich.edu>
2024-03-14 12:31:05 -0400

    creating a footer and including it in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { NavBar } from "./components/NavBar";
 //import { Skills } from "./components/Skills";
 //import { Projects } from "./components/Projects";
 //import { Contact } from "./components/Contact";
-//import { Footer } from "./components/Footer";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
