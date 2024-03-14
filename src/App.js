5f559ac00b11bf3f1d7cf5d455b7fcf7066177d2
gsalgado <gsalgado@umich.edu>
2024-03-14 10:31:29 -0400

    navBar try

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+//import { Banner } from "./components/Banner";
+//import { Skills } from "./components/Skills";
+//import { Projects } from "./components/Projects";
+//import { Contact } from "./components/Contact";
+//import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +42,4 @@ function App() {
 }
 
 export default App;
+*/
