6e4aa7a4c4fb224328487f5598b76f5540dd6b9c
gsalgado <gsalgado@umich.edu>
2024-03-22 07:01:26 -0400

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,43 +1,33 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
-//import logo from "../assets/img/logo.svg";
 import gitIcon from "../assets/img/github-icon.png.webp";
 import lInIcon from "../assets/img/lIn4-removebg-preview.png";
-
-//import navIcon2 from "../assets/img/nav-icon2.svg";
-//import navIcon3 from "../assets/img/nav-icon3.svg";
-
-//import linkedInLogo from './linkedin_logo.png'; // Import LinkedIn logo image
-//import githubLogo from './github_logo.png'; // Import GitHub logo image                         <p>Copyright 2024. All Rights Reserved</p>
 
 
 export const Footer = () => {
     return (
-        <div>
-            {/* Divider line */}
+        <footer className="footer">
             <div className="footer-divider"></div>
-            <footer className="footer">
-                <Container>
-                    <Row className="align-items-center gx-2">
-                        <Col sm={3} className="text-center text-sm-end">
-                            {/* LinkedIn button */}
-                            <a href="https://www.linkedin.com/in/gabriela-lopez-salgado" target="_blank" rel="noopener noreferrer">
-                                <button className="social-button"><img src={lInIcon} alt="LinkedIn" /></button></a>
-                        </Col>
-                        <Col sm={3} className="text-center text-sm-ensd">
-                            {/* GitHub button */}
-                            <a href="https://github.com/glopezsalgado" target="_blank" rel="noopener noreferrer">
-                                <button className="social-button"><img src={gitIcon} alt="GitHub" /></button></a>
-                        </Col>
-                        <Col sm={3} className="text-center text-sm-end">
-                            <button className="contact" onClick={() => window.open('mailto:gsalgado@umich.edu')}>gsalgado@umich.edu</button>
-                        </Col>
-                        <Col sm={3} className="text-center text-sm-end">
-                            <button className="contact" onClick={() => window.open('tel:+15179186443')}>517 - 918 - 6443</button>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>
-        </div>
+
+            <Container>
+                <Row className="align-items-center gx-2">
+                    <Col sm={3} className="text-center text-sm-end">
+                        {/* LinkedIn button */}
+                        <a href="https://www.linkedin.com/in/gabriela-lopez-salgado" target="_blank" rel="noopener noreferrer">
+                            <button className="lIn-social-button"><img src={lInIcon} alt="LinkedIn" /></button></a>
+                    </Col>
+                    <Col sm={3} className="text-center text-sm-ensd">
+                        {/* GitHub button */}
+                        <a href="https://github.com/glopezsalgado" target="_blank" rel="noopener noreferrer">
+                            <button className="git-social-button"><img src={gitIcon} alt="GitHub" /></button></a>
+                    </Col>
+                    <Col sm={3} className="text-center text-sm-end">
+                        <button className="email" onClick={() => window.open('mailto:gsalgado@umich.edu')}>gsalgado@umich.edu</button>
+                    </Col>
+                    <Col sm={3} className="text-center text-sm-end">
+                        <button className="phone" onClick={() => window.open('tel:+15179186443')}>517 - 918 - 6443</button>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
