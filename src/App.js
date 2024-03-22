06a028d196199032de4c7a02b60748302e700062
gsalgado <gsalgado@umich.edu>
2024-03-22 04:34:35 -0400

    fixing text headers in navbar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import { Banner } from "./components/Banner";
 //import { Skills } from "./components/Skills";
 //import { Projects } from "./components/Projects";
 //import { Contact } from "./components/Contact";
-//import { Footer } from "./components/Footer";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Footer />
     </div>
   );
 }
