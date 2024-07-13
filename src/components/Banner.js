44cb28ece47dbf3699cffe843a3a4eefe8c55711
gsalgado <gsalgado@umich.edu>
2024-07-13 16:48:43 -0700

    fixing  resume to website

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -69,18 +69,25 @@ export const Banner = () => {
                             <button onClick={resumeButtonClick}>See Resume</button>
                         </div>
                     </Col>
-                </Row>
-                {showResume && (
-                    <Row>
-                        <Col xs={12} md={6}>
-                            <iframe
-                                src={resume}
-                                style={{ width: '100%', height: '500px' }}
-                                title="Resume PDF Viewer"
-                            ></iframe>
-                        </Col>
-                    </Row>
-                )}
+
+                    {showResume && (
+                        <Row>
+                            <Col xs={12} md={6}>
+                                <div className="below-image">
+                                    <h1><span>My name is </span><span className="name">Gabriela Ivonne Lopez-Salgado</span></h1>
+                                    <h1>{`I'm a`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Mobile iOS Developer", "Test Engineer", "Researcher", "Software Engineer", "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                                    <p>My passion lies in computing all of the possible edge cases within a system. I have experience with electronic circuit design, mobile app development, research, embedded systems, and technical documentation.</p>
+                                </div>
+                            </Col>
+                            <Col xs={12} md={6}>
+                                <iframe
+                                    src={resume}
+                                    style={{ width: '100%', height: '500px' }}
+                                    title="Resume PDF Viewer"
+                                ></iframe>
+                            </Col>
+                        </Row>
+                    )}
 
 
             </Container>
